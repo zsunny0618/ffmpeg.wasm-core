90c86abe534e2790d517796121ffce1dce161766
Jerome Wu <jeromewus@gmail.com>
2019-10-17 22:21:47 +0800

    Add libwavpack, libmp3lame and libfdk-aac

--- build-js.sh
+++ build-js.sh
@@ -1,11 +1,52 @@
 #!/bin/bash -x
 
-configure_js() {
+set -e -o pipefail
+
+NPROC=$(grep -c ^processor /proc/cpuinfo)
+BUILD_DIR=$PWD/build
+
+install_deps() {
+  apt-get update
+  apt-get install -y autoconf libtool
+}
+
+build_libwavpack() {
+  cd third_party/WavPack
+  emconfigure ./autogen.sh \
+    --disable-asm \
+    --prefix=$BUILD_DIR
+  emmake make install -j${NPROC}
+  cd -
+}
+
+build_libfdk_aac() {
+  cd third_party/fdk-aac
+  ./autogen.sh
+  emconfigure ./configure --prefix=$BUILD_DIR
+  emmake make install -j${NPROC}
+  cd -
+}
+
+build_libmp3lame() {
+  cd third_party/lame
+  emconfigure ./configure --prefix=$BUILD_DIR
+  emmake make install -j${NPROC}
+  cd -
+}
+
+configure_ffmpeg() {
   emconfigure ./configure \
     --disable-x86asm \
     --disable-inline-asm \
     --disable-doc \
     --disable-stripping \
+    --disable-ffprobe \
+    --disable-ffmpeg \
+    --enable-libmp3lame \
+    --enable-libfdk-aac \
+    --prefix=$BUILD_DIR \
+    --extra-cflags=-I$BUILD_DIR/include \
+    --extra-ldflags=-L$BUILD_DIR/lib \
     --nm="llvm-nm -g" \
     --ar=emar \
     --cc=emcc \
@@ -14,24 +55,33 @@ configure_js() {
     --dep-cc=emcc
 }
 
-make_js() {
+make_ffmpeg() {
   NPROC=$(grep -c ^processor /proc/cpuinfo)
   emmake make -j${NPROC}
 }
 
-build_js() {
+build_ffmpegjs() {
   emcc \
-    -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample \
+    -I. -I$BUILD_DIR/include \
+    -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample -L$BUILD_DIR/lib \
     -Qunused-arguments \
-    -o ffmpeg.js fftools/ffmpeg_opt.o fftools/ffmpeg_filter.o fftools/ffmpeg_hw.o fftools/cmdutils.o fftools/ffmpeg.o \
-    -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -pthread \
-    -s TOTAL_MEMORY=33554432
+    -o javascript/ffmpeg-core.js libffmpeg/transcoding.c \
+    -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm -lmp3lame -lfdk-aac \
+    -s MODULARIZE=1 \
+    -s EXPORTED_FUNCTIONS="[_av_log_set_level, _transcoding]" \
+    -s EXTRA_EXPORTED_RUNTIME_METHODS="[cwrap, FS, getValue, setValue]" \
+    -s TOTAL_MEMORY=33554432 \
+    -s ALLOW_MEMORY_GROWTH=1
 }
 
 main() {
-  configure_js
-  make_js
-  build_js
+  install_deps
+  build_libwavpack
+  build_libfdk_aac
+  build_libmp3lame
+  configure_ffmpeg
+  make_ffmpeg
+  build_ffmpegjs
 }
 
 main "$@"
---
+++ libffmpeg/transcoding.h
@@ -0,0 +1,6 @@
+#ifndef FFMPEG_TRANSCODING_H
+#define FFMPEG_TRANSCODING_H
+
+int transcoding(char *input_filename, char *output_filename);
+
+#endif /* FFMPEG_TRANSCODING_H */
