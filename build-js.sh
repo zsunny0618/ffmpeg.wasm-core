24934d162ba435a808c33d04c297a30d48a21216
Jerome Wu <jeromewus@gmail.com>
2019-10-24 07:43:14 +0800

    Add SINGLE_FILE flag

--- build-js.sh
+++ build-js.sh
@@ -29,6 +29,7 @@ build_ffmpegjs() {
     -o javascript/ffmpeg-core.js fftools/ffmpeg_opt.o fftools/ffmpeg_filter.o fftools/ffmpeg_hw.o fftools/cmdutils.o fftools/ffmpeg.o \
     -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm \
     -s MODULARIZE=1 \
+    -s SINGLE_FILE=1 \
     -s EXPORTED_FUNCTIONS="[_ffmpeg]" \
     -s EXTRA_EXPORTED_RUNTIME_METHODS="[cwrap, FS, getValue, setValue]" \
     -s TOTAL_MEMORY=33554432 \
