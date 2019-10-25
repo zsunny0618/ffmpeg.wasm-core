0b9c5808156bfd539dcaccf50196e49d42c88c6a
Jerome Wu <jeromewus@gmail.com>
2019-10-25 12:18:39 +0800

    Disable pthreads and add SDL ports

--- build-js.sh
+++ build-js.sh
@@ -4,11 +4,13 @@ set -e -o pipefail
 
 configure_ffmpeg() {
   emconfigure ./configure \
+    --disable-pthreads \
     --disable-x86asm \
     --disable-inline-asm \
     --disable-doc \
     --disable-stripping \
     --disable-ffprobe \
+    --disable-ffplay \
     --nm="llvm-nm -g" \
     --ar=emar \
     --cc=emcc \
@@ -28,6 +30,7 @@ build_ffmpegjs() {
     -Qunused-arguments -Oz \
     -o javascript/ffmpeg-core.js fftools/ffmpeg_opt.o fftools/ffmpeg_filter.o fftools/ffmpeg_hw.o fftools/cmdutils.o fftools/ffmpeg.o \
     -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lm \
+    -s USE_SDL=2 \
     -s MODULARIZE=1 \
     -s SINGLE_FILE=1 \
     -s EXPORTED_FUNCTIONS="[_ffmpeg]" \
