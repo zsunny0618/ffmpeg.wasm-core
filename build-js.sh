ace1d0e72bcfd2802ce57d63813c99d824397258
Jerome Wu <jeromewus@gmail.com>
2019-11-05 23:08:30 +0800

    Add x265

--- build-js.sh
+++ build-js.sh
@@ -2,7 +2,9 @@
 
 set -e -o pipefail
 
-BUILD_DIR=$PWD/build
+NPROC=$(grep -c ^processor /proc/cpuinfo)
+ROOT_DIR=$PWD
+BUILD_DIR=$ROOT_DIR/build
 
 build_x264() {
   cd third_party/x264
@@ -10,14 +12,29 @@ build_x264() {
     --disable-asm \
     --disable-thread \
     --prefix=$BUILD_DIR
-  emmake make install-lib-static
-  cd -
+  emmake make install-lib-static -j${NPROC}
+  cd $ROOT_DIR
+}
+
+build_x265() {
+  cd third_party/x265/source
+  rm -rf build
+  mkdir build
+  cd build
+  emmake cmake .. \
+    -G "Unix Makefiles" \
+    -DENABLE_SHARED:bool=off \
+    -DSTATIC_LINK_CRT:bool=on \
+    -DCMAKE_INSTALL_PREFIX=$BUILD_DIR
+  emmake make install -j${NPROC}
+  cd $ROOT_DIR
 }
 
 configure_ffmpeg() {
-  emconfigure ./configure \
+ PKG_CONFIG_PATH=/src/build/lib/pkgconfig emconfigure ./configure \
     --enable-gpl \
     --enable-libx264 \
+    --enable-libx265 \
     --disable-pthreads \
     --disable-x86asm \
     --disable-inline-asm \
@@ -39,7 +56,6 @@ configure_ffmpeg() {
 }
 
 make_ffmpeg() {
-  NPROC=$(grep -c ^processor /proc/cpuinfo)
   emmake make -j${NPROC}
 }
 
@@ -49,7 +65,7 @@ build_ffmpegjs() {
     -Llibavcodec -Llibavdevice -Llibavfilter -Llibavformat -Llibavresample -Llibavutil -Llibpostproc -Llibswscale -Llibswresample -Llibpostproc -L${BUILD_DIR}/lib \
     -Qunused-arguments -Oz \
     -o dist/ffmpeg-core.js fftools/ffmpeg_opt.c fftools/ffmpeg_filter.c fftools/ffmpeg_hw.c fftools/cmdutils.c fftools/ffmpeg.c \
-    -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lx264 \
+    -lavdevice -lavfilter -lavformat -lavcodec -lswresample -lswscale -lavutil -lpostproc -lm -lx264 -lx265 \
     --closure 1 \
     --pre-js javascript/prepend.js \
     -s USE_SDL=2 \
@@ -58,11 +74,13 @@ build_ffmpegjs() {
     -s EXPORTED_FUNCTIONS="[_ffmpeg]" \
     -s EXTRA_EXPORTED_RUNTIME_METHODS="[cwrap, FS, getValue, setValue]" \
     -s TOTAL_MEMORY=33554432 \
+    -s ASSERTIONS=1 \
     -s ALLOW_MEMORY_GROWTH=1
 }
 
 main() {
-  build_x264
+  #build_x264
+  build_x265
   configure_ffmpeg
   make_ffmpeg
   build_ffmpegjs
