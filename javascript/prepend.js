1fc6bf8682cf8f067ddd145b02aa76cbcaed0ffc
jeromewu <jeromewus@gmail.com>
2019-12-02 13:16:08 +0000

    Add preRun to mount IDBFS and NODEFS

--- javascript/prepend.js
+++ javascript/prepend.js
@@ -3,3 +3,17 @@ var logger = function(){}
 Module['setLogger'] = function(_logger) { logger = _logger; };
 Module['print'] = function(message) { logger(message, 'stdout'); };
 Module['printErr'] = function(message) { logger(message, 'stderr'); };
+Module['preRun'] = [
+  function() {
+    FS.mkdir('/data');
+    if (typeof process === 'object' && typeof require === 'function') {
+      try {
+        require('fs').mkdirSync('./data');
+      } catch(e) {}
+      FS.mount(NODEFS, { root: '.' }, '/data');
+    } else if (typeof importScripts === 'function') {
+      FS.mount(IDBFS, {}, '/data');
+      FS.writeFile('/data/.DUMMY', new Uint8Array([]));
+    }
+  },
+];
