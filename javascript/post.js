577814953b87d842ae3e41dfa4c091c0a233694e
Jerome Wu <jeromewus@gmail.com>
2019-12-03 19:36:29 +0800

    Fix syncfs bug

--- javascript/post.js
+++ javascript/post.js
@@ -1,6 +1,6 @@
 Module['syncfs'] = function(populate) {
   return new Promise(function(resolve, reject) {
-    Module.FS.syncfs(populate, function(err) {
+    FS.syncfs(populate, function(err) {
       if (err) {
         reject(err);
       } else {
