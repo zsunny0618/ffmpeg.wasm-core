47977863549d9ad0d18e8c9edde0f7bb169ee396
Jerome Wu <jeromewus@gmail.com>
2020-04-26 19:58:28 +0800

    Optimize build scripts

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -14,19 +14,12 @@ check_command() {
 }
 
 build() {
-  if [ "$TTY" = "no" ]; then
-    docker run \
-      --rm \
-      -v ${PWD}:/src \
-      trzeci/emscripten:${EMSCRIPTEN_VERSION} \
-      sh -c "bash ./build-js.sh"
-  else
-    docker run -it \
-      --rm \
-      -v ${PWD}:/src \
-      trzeci/emscripten:${EMSCRIPTEN_VERSION} \
-      sh -c "bash ./build-js.sh"
-  fi
+  docker run \
+    --name emscripten-tessjs \
+    --rm \
+    -v ${PWD}:/src \
+    trzeci/emscripten:${EMSCRIPTEN_VERSION} \
+    sh -c "bash ./build-js.sh"
 }
 
 main() {
