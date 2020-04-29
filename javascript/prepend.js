1d0b3c8cf5b8f6ce850f47c0445fcf55fc8e1732
Jerome Wu <jeromewus@gmail.com>
2020-04-29 09:16:32 +0800

    Fix worker origin issue

--- javascript/prepend.js
+++ javascript/prepend.js
@@ -3,6 +3,14 @@ var logger = function(){}
 Module['setLogger'] = function(_logger) { logger = _logger; };
 Module['print'] = function(message) { logger({ message, type: 'ffmpeg-stdout' }); };
 Module['printErr'] = function(message) { logger({ message, type: 'ffmpeg-stderr' }); };
+Module['locateFile'] = function(path, prefix) {
+  if (typeof window !== 'undefined'
+    && typeof window.FFMPEG_CORE_WORKER_SCRIPT !== 'undefined'
+    && path.endsWith('ffmpeg-core.worker.js')) {
+    return window.FFMPEG_CORE_WORKER_SCRIPT;
+  }
+  return prefix + path;
+}
 /*
 Module['preRun'] = [
   function() {
