57b11e9a91a2bd506cd70df5086d04051df58ef5
Jerome Wu <jeromewus@gmail.com>
2020-08-31 15:22:40 +0800

    Add build ffmpeg.wasm script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # verify Emscripten version
 emcc -v
 
-# configure FFMpeg with Emscripten
+# configure FFmpeg with Emscripten
 emconfigure ./configure \
   --disable-x86asm \
   --nm="llvm-nm -g" \
@@ -14,3 +14,7 @@ emconfigure ./configure \
   --cxx=em++ \
   --objcc=emcc \
   --dep-cc=emcc
+
+# build FFmpeg.wasm
+
+emmake make -j
