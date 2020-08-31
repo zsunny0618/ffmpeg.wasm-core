ae99c7c898b18b80f73cbf93ce22784be841e951
Jerome Wu <jeromewus@gmail.com>
2020-08-31 15:46:16 +0800

    Fix syntax error

--- build.sh
+++ build.sh
@@ -4,18 +4,20 @@
 emcc -v
 
 # configure FFmpeg with Emscripten
-emconfigure ./configure \
-  --disable-x86asm \
-  --disable-inline-asm \    # Disable inline asm
-  --disable-doc \           # Disable document generation
-  --nm="llvm-nm -g" \
-  --ar=emar \
-  --as=llvm-as \
-  --ranlib=llvm-ranlib \
-  --cc=emcc \
-  --cxx=em++ \
-  --objcc=emcc \
+FLAGS=(
+  --disable-x86asm
+  --disable-inline-asm    #Disable inline asm
+  --disable-doc           #Disable document generation
+  --nm="llvm-nm -g"
+  --ar=emar
+  --as=llvm-as
+  --ranlib=llvm-ranlib
+  --cc=emcc
+  --cxx=em++
+  --objcc=emcc
   --dep-cc=emcc
+)
+emconfigure ./configure "${FLAGS[@]}"
 
 # build FFmpeg.wasm
 emmake make -j
