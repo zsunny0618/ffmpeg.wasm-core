648e2f45163e318e6beb60748579d3c95593bde5
Jerome Wu <jeromewus@gmail.com>
2020-08-31 17:20:22 +0800

    Update build-with-docker.sh

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -x
 
-FLAGS=""
-# Attach TTY only when available, this is for running in Gihub Actions
-if [ -t 1 ]; then FLAGS="-it"; fi
-
 docker pull gcc:8
-docker run $FLAGS \
+docker run \
   -v $PWD:/usr/src \
   gcc:8 \
   sh -c 'cd /usr/src && bash ./build.sh'
