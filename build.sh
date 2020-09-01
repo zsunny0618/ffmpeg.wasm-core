f043a0e1a773eb5c349dbc31c11350f1bfd38d05
Jerome Wu <jeromewus@gmail.com>
2020-09-01 17:07:44 +0800

    Fix build scripts & update workflows

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ ROOT=$(dirname $0)
 # verify Emscripten version
 emcc -v
 # configure FFmpeg with Emscripten
-# $ROOT/wasm/build-scripts/configure.sh
+$ROOT/wasm/build-scripts/configure.sh
 # build dependencies
-# $ROOT/wasm/build-scripts/make.sh
+$ROOT/wasm/build-scripts/make.sh
 # build ffmpeg.wasm
 $ROOT/wasm/build-scripts/build-ffmpeg.sh
