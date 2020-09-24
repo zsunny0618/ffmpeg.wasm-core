1bc6c17f9cfd13bd5d66baa2b91f8e80870c25e9
Jerome Wu <jeromewus@gmail.com>
2020-09-24 18:24:44 +0800

    Add a custom message to mark the end of operation

--- fftools/cmdutils.c
+++ fftools/cmdutils.c
@@ -135,6 +135,12 @@ void exit_program(int ret)
     if (program_exit)
         program_exit(ret);
 
+    /*
+     * Print an unique message here to detect
+     * end of operation in JavaScript.
+     */
+    printf("FFMPEG_END\n");
+
     exit(ret);
 }
 
