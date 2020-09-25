74e8907ee3006b45ada99511acd62985de7da2fc
Jerome Wu <jeromewus@gmail.com>
2020-09-25 09:54:05 +0800

    Test ignore closure in Mac

--- wasm/build-scripts/build-x264.sh
+++ wasm/build-scripts/build-x264.sh
@@ -5,6 +5,14 @@ set -eo pipefail
 ROOT=$1
 BUILD_DIR=$2
 
+OPT_FLAGS="-O3 --closure 1"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # As closure compiler requires java 6 and
+  # I don't find a way to install it in github actions
+  OPT_FLAGS="-O3"
+fi
+
 cd $ROOT
 ARGS=(
   --prefix=$BUILD_DIR                                 # install library in a build directory for FFmpeg to include
@@ -12,7 +20,7 @@ ARGS=(
   --enable-static                                     # enable building static library
   --disable-cli                                       # disable cli tools
   --disable-asm                                       # disable asm optimization
-  --extra-cflags="-s USE_PTHREADS=1 -O3 --closure 1"  # flags to use pthread and code optimization
+  --extra-cflags="-s USE_PTHREADS=1 $OPT_FLAGS"  # flags to use pthread and code optimization
 )
 emconfigure ./configure "${ARGS[@]}"
 emmake make install-lib-static -j4
