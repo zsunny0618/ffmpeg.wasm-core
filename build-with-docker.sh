a4417eb3bc684c1adfd0639599f8fcb193d38870
Jerome Wu <jeromewus@gmail.com>
2020-10-28 16:16:21 +0800

    Upgrade emscripten to 2.0.8

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-EM_VERSION=1.39.18-upstream
+EM_VERSION=2.0.8
 
-docker pull trzeci/emscripten:$EM_VERSION
+docker pull emscripten/emsdk:$EM_VERSION
 docker run \
   --rm \
   -v $PWD:/src \
   -v $PWD/wasm/cache:/emsdk_portable/.data/cache/wasm \
-  trzeci/emscripten:$EM_VERSION \
+  emscripten/emsdk:$EM_VERSION \
   sh -c 'bash ./build.sh'
