e3b3842132f6b0f6e4e755a0487a293de128bb85
Jerome Wu <jeromewus@gmail.com>
2020-11-03 17:34:33 +0800

    Revert prepend

--- wasm/src/prepend.js
+++
@@ -1,8 +0,0 @@
-Module['locateFile'] = function(path, prefix) {
-  if (typeof window !== 'undefined'
-    && typeof window.FFMPEG_CORE_WORKER_SCRIPT !== 'undefined'
-    && path.endsWith('ffmpeg-core.worker.js')) {
-    return window.FFMPEG_CORE_WORKER_SCRIPT;
-  }
-  return prefix + path;
-}
