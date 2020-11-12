3b65cec011c469a328df6371d38a6f19d7ce7df5
Jerome Wu <jeromewus@gmail.com>
2020-11-12 10:27:21 +0800

    Update mp4 scale build script

--- wasm/build-scripts/build-ffmpeg.mp4-scale.sh
+++ wasm/build-scripts/build-ffmpeg.mp4-scale.sh
@@ -21,7 +21,7 @@ FLAGS=(
   -s EXPORT_NAME="createFFmpegCore"             # assign export name for browser
   -s EXPORTED_FUNCTIONS="[_main, _proxy_main]"  # export main and proxy_main funcs
   -s EXTRA_EXPORTED_RUNTIME_METHODS="[FS, cwrap, ccall, setValue, writeAsciiToMemory]"   # export preamble funcs
-  -s INITIAL_MEMORY=1073741824                  # 1073741824 bytes = 1 GB
+  -s INITIAL_MEMORY=2146435072                  # 64 KB * 1024 * 16 * 2047 = 2146435072 bytes ~= 2 GB
   $OPTIM_FLAGS
 )
 echo "FFMPEG_EM_FLAGS=${FLAGS[@]}"
