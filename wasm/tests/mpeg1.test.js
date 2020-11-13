12c78a7b71b8c7832ce36d31ac98029e0089546e
Jerome Wu <jeromewus@gmail.com>
2020-11-13 18:00:59 +0800

    Add tests for mpeg1, mpeg2 and flac, increase timeout

---
+++ wasm/tests/mpeg1.test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { TIMEOUT } = require('./config');
+const { runFFmpeg } = require('./utils');
+const IN_FILE_NAME = 'video-1s.avi';
+const OUT_FILE_NAME = 'video.mpeg';
+const FILE_SIZE = 53248;
+let aviData = null;
+
+beforeAll(() => {
+  aviData = Uint8Array.from(fs.readFileSync(path.join(__dirname, 'data', IN_FILE_NAME)));
+});
+
+test('transcode avi to mpeg1', async () => {
+  const args = ['-i', IN_FILE_NAME, '-c:v', 'mpeg1video', OUT_FILE_NAME];
+  const { fileSize } = await runFFmpeg(IN_FILE_NAME, aviData, args, OUT_FILE_NAME);
+  expect(fileSize).toBe(FILE_SIZE);
+}, TIMEOUT);
